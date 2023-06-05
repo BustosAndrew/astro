c78566df6c5a7794599b7a624a5824967fc05049
Andrew Bustos <infernious@live.com>
2023-06-04 17:28:36 -0700

    Updated site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://astro-nine-azure.vercel.app/",
+  site: "https://soloiron.dev/",
   integrations: [
     tailwind(),
     image({
