3a5ea8f09e71c5e98f19bed8de616551e7be1575
Andrew Bustos <infernious@live.com>
2025-03-25 15:38:41 -0700

    feat: integrate Tailwind CSS and update configurations
    - Added global CSS file to import Tailwind CSS.
    - Updated Layout.astro to include the global CSS.
    - Modified astro.config.mjs to use Vercel adapter and Tailwind CSS plugin.
    - Adjusted tsconfig.json to include and exclude specific directories.
    - Updated package.json to reflect new dependencies and versions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://soloiron.dev/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [mdx(), sitemap()],
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
